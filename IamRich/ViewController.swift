a81e98698f094865b89fe0f39141ba8c285a0685
Lando Winzen <winzenlando@gmail.com>
2025-10-23 18:16:48 +0200

    Initial Commit

---
+++ IamRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IamRich
+//
+//  Created by Lando Winzen on 23.10.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
